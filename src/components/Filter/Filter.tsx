b6965ba3439144edc6c5c81d082d7e1016c70c8e
SandroK0 <sandro.kardava.tech@gmail.com>
2025-03-16 21:18:36 +0400

    fix filter dropdown with delayed rendering
    Moved fetch logic to the parent component, which is always rendered on the home page. This resolves the issue with the filter dropdown and removes delay for smoother interaction.

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,23 +1,70 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import FilterItem from "../FilterItem/FilterItem";
 import FilterDropdown from "../FilterDropdown/FilterDropdown";
 import styles from "./Filter.module.css";
-import { FilterType, SelectedFilters } from "../../Types";
+import { Employee, FilterType, SelectedFilters } from "../../Types";
+import { getDepartments, getEmployees, getPriorities } from "../../api/api";
 
 interface FilterProps {
   selectedFilters: SelectedFilters;
   setSelectedFilters: React.Dispatch<React.SetStateAction<SelectedFilters>>;
 }
 
+function handleEmployeeData(
+  employees: Employee[]
+): { id: number; name: string }[] {
+  return employees.map(({ id, avatar, name, surname }) => ({
+    id,
+    avatar,
+    name: `${name} ${surname}`,
+  }));
+}
+
 export default function Filter(props: FilterProps) {
   const [filterBy, setFilterBy] = useState<FilterType | "">("");
+  const filterContRef = useRef<HTMLDivElement>(null);
 
   function handleFilterClick(filter: FilterType) {
     setFilterBy(filterBy === filter ? "" : filter);
   }
 
+  const [priorityFiltersData, setPriorityFiltersData] = useState<any[]>([]);
+  const [departmentFiltersData, setDepartmentFiltersData] = useState<any[]>([]);
+  const [employeeFiltersData, setEmployeeFiltersData] = useState<any[]>([]);
+
+  async function getFiltersData() {
+    let priorities = await getPriorities();
+    let departments = await getDepartments();
+    let employees = await getEmployees();
+    employees = handleEmployeeData(employees);
+
+    setPriorityFiltersData(priorities);
+    setEmployeeFiltersData(employees);
+    setDepartmentFiltersData(departments);
+  }
+
+  useEffect(() => {
+    getFiltersData();
+  }, []);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        filterContRef.current &&
+        !filterContRef.current.contains(event.target as Node)
+      ) {
+        setFilterBy("");
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className={styles.filterCont}>
+    <div className={styles.filterCont} ref={filterContRef}>
       <div className={styles.filters}>
         {(["departments", "priorities", "employees"] as FilterType[]).map(
           (filter) => (
@@ -33,6 +80,13 @@ export default function Filter(props: FilterProps) {
       {filterBy && (
         <FilterDropdown
           filterBy={filterBy}
+          filtersData={
+            filterBy === "priorities"
+              ? priorityFiltersData
+              : filterBy === "departments"
+              ? departmentFiltersData
+              : employeeFiltersData
+          }
           selectedFilters={props.selectedFilters}
           setSelectedFilters={props.setSelectedFilters}
           close={() => setFilterBy("")}
--- src/components/FilterDropdown/FilterDropdown.tsx
+++ src/components/FilterDropdown/FilterDropdown.tsx
@@ -1,64 +1,25 @@
 import { useEffect, useState } from "react";
-import { getDepartments, getEmployees, getPriorities } from "../../api/api";
 import styles from "./FilterDropdown.module.css";
-import { Employee, FilterType, SelectedFilters } from "../../Types";
+import { FilterType, SelectedFilters } from "../../Types";
 
 interface FilterDropdownProps {
   filterBy: FilterType;
   selectedFilters: SelectedFilters;
+  filtersData: any[];
   setSelectedFilters: React.Dispatch<React.SetStateAction<SelectedFilters>>;
   close: () => void;
 }
 
-function handleEmployeeData(
-  employees: Employee[]
-): { id: number; name: string }[] {
-  return employees.map(({ id, name, surname }) => ({
-    id,
-    name: `${name} ${surname}`,
-  }));
-}
-
 export default function FilterDropdown(props: FilterDropdownProps) {
-  const { filterBy, selectedFilters, setSelectedFilters, close } = props;
+  const { filterBy, selectedFilters, setSelectedFilters, close, filtersData } =
+    props;
   const [selectedFiltersLocal, setSelectedFiltersLocal] = useState<
     Array<string>
   >([]);
-  const [filters, setFilters] = useState<any[]>([]);
   const [msg, setMsg] = useState<string>("");
 
-  async function getFiltersData(filterBy: FilterType) {
-    try {
-      let data;
-      switch (filterBy) {
-        case "priorities":
-          data = await getPriorities();
-          break;
-        case "departments":
-          data = await getDepartments();
-          break;
-        case "employees":
-          data = await getEmployees();
-
-          if (!data) {
-            setMsg("თანამშრომლები ჯერ არ დაგიმატებიათ");
-            return;
-          }
-          data = handleEmployeeData(data);
-          break;
-        default:
-          return;
-      }
-      setFilters(data);
-    } catch (err: any) {
-      console.log(err);
-    }
-  }
-
   useEffect(() => {
-    setFilters([]);
     setMsg("");
-    getFiltersData(filterBy);
     setSelectedFiltersLocal(selectedFilters[filterBy]);
   }, [filterBy]);
 
@@ -82,8 +43,8 @@ export default function FilterDropdown(props: FilterDropdownProps) {
   return (
     <div className={styles.filterDropdown}>
       <div className={styles.filterCont}>
-        {filters.length > 0 &&
-          filters.map((el: any) => (
+        {filtersData.length > 0 &&
+          filtersData.map((el: any) => (
             <div className={styles.filter} key={el.id}>
               <input
                 className={styles.checkbox}
@@ -91,6 +52,9 @@ export default function FilterDropdown(props: FilterDropdownProps) {
                 checked={selectedFiltersLocal.includes(el.name)}
                 onChange={() => handleCheckboxChange(el.name)}
               />
+              {el.avatar && (
+                <img src={el.avatar} className={styles.avatar} alt="avatar" />
+              )}
               <div>{el.name}</div>
             </div>
           ))}
